79092cec0976788a1d2120bbcddde5e3bdb49c21
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-05-10 15:38:58 +0545

    google provider in sign to direct login

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -12,22 +12,21 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
-  // const session = await getServerSession(authOptions);
-  // if (!session) {
-  //   return new NextResponse(
-  //     JSON.stringify({ status: "fail", message: "You are not logged in" }),
-  //     { status: 401 }
-  //   );
-  // }
-  // const { user } = await getUserByEmail(session.user?.email as string);
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ status: "fail", message: "You are not logged in" }),
+      { status: 401 }
+    );
+  }
+  const { user } = await getUserByEmail(session.user?.email as string);
   const body = await request.json();
   const newPost = {
     title: body.title,
     content: body.content,
-    //userId: user?.id as string,
+    userId: user?.id as string,
     published: false,
   };
-  //@ts-ignore
   const post = await createPost(newPost);
   return NextResponse.json({ data: post });
 }
--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -2,7 +2,7 @@
 import { signIn } from "next-auth/react";
 const Login = () => {
   return (
-    <button className="navLink" onClick={() => signIn()}>
+    <button className="navLink" onClick={() => signIn("google")}>
       Login
     </button>
   );
