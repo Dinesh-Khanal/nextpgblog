11f4f75e300c117f8e7c4737f859ceeca0823dac
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-05-14 21:58:13 +0545

    next js updated and change on prisma file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient = new PrismaClient();
+//const prisma: PrismaClient = new PrismaClient();
 
-export default prisma;
+//export default prisma;
+
+import { PrismaClient } from "@prisma/client"
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const client = globalThis.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+
+export default client
